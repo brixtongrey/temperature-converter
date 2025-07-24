2991634f1461dd01a31b5aff80954c6c40018b6a
Brixtongrey <brixtonaruda@gmail.com>
2025-07-24 10:02:39 -0400

    feat: created new function

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 function convertToCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
 }
+
+function describeTemperature(fahrenheit) {
+  const celsius = convertToCelsius(fahrenheit);
+
+    if (celsius < 0) {
+    console.log("very cold");
+  } else if (celsius < 20) {
+    console.log("cold");
+  } else if (celsius < 30) {
+    console.log("warm");
+  } else if (celsius < 40) {
+    console.log("hot");
+  } else if (celsius >= 40) {
+    console.log("very hot");
+  }
+}
