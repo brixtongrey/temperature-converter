2d95790a955d7a7f708dbe1c38bf8ad9e3f40e10
Brixtongrey <brixtonaruda@gmail.com>
2025-07-24 10:12:45 -0400

    fix: else-if statement

--- index.js
+++ index.js
@@ -6,21 +6,41 @@ function convertToCelsius(fahrenheit) {
 function describeTemperature(fahrenheit) {
   const celsius = convertToCelsius(fahrenheit);
 
-    if (celsius < 0) {
-    console.log("very cold");
+//     if (celsius < 0) {
+//     console.log("very cold");
+//   } else if (celsius < 20) {
+//     console.log("cold");
+//   } else if (celsius < 30) {
+//     console.log("warm");
+//   } else if (celsius < 40) {
+//     console.log("hot");
+//   } else if (celsius >= 40) {
+//     console.log("very hot");
+//   }
+// }
+
+  if (celsius < 0) {
+     return "very cold";
   } else if (celsius < 20) {
-    console.log("cold");
+    return "cold";;
   } else if (celsius < 30) {
-    console.log("warm");
+    return "warm";
   } else if (celsius < 40) {
-    console.log("hot");
+    return "hot";
   } else if (celsius >= 40) {
-    console.log("very hot");
+    return "very hot";
   }
 }
-
 
 const input = prompt("Enter temperature in Fahrenheit:");
 
 const fahrenheitTemp = Number(input);
+
+if (isNaN(fahrenheitTemp)) {
+  alert ("Please enter a valid number.");
+} else {
+  const celsiusTemp = convertToCelsius(fahrenheitTemp);
+  const description = describeTemperature(fahrenheitTemp);
+    alert(`The temperature in Celsius is ${celsiusTemp.toFixed(1)}°C, which is considered ${description}.`);
+}
 
